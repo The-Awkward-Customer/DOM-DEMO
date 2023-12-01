5e508989507705acc0d475d7691a1a3a6951062d
PETER ABBOTT <abbotsayshi@gmail.com>
2023-12-01 09:18:21 +0000

    first real commit bruv

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const body = document.querySelector("body");
   console.log(body);
 
-  const h1 = document.querySelector("h1");
-  console.log(h1);
+  // const h1 = document.querySelector("h1");
+  // console.log(h1);
 
-  const h2 = document.querySelector("h2");
-  h2.textContent = "I changed this using JS";
+  // const h2 = document.querySelector("h2");
+  // h2.textContent = "I changed this using JS";
 });
 
 // const body = document.querySelector("body");
