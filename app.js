5bb4c4ddfc96ab4e74c5ce21fe43a0c58ce16d77
PETER ABBOTT <abbotsayshi@gmail.com>
2023-12-01 10:27:19 +0000

    finished workshop

--- app.js
+++ app.js
@@ -14,3 +14,21 @@ console.log(h2);
 //changes the h2 textContent
 const h2Text = document.querySelector("h2");
 h2Text.textContent = "I changed this using JS";
+
+const changeImage = document.querySelector("img");
+changeImage.src = "./images/eclipse.jpg";
+
+// points at the body (already declared above at line 6)
+//const body = document.querySelector("body");
+
+// creates a new paragraph element
+const pNew = document.createElement("p");
+// sets the content for our new p tag
+pNew.textContent = "I'm a new paragraph YAY";
+
+// adds the p tag to the end of the body
+body.appendChild(pNew);
+
+const h3 = document.querySelector("h3");
+
+h3.remove();
