51044c18fdfc3d29e07a8d3e3fde15af375f42b0
PETER ABBOTT <abbotsayshi@gmail.com>
2023-12-01 09:55:49 +0000

    added image assets

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 console.log("hello there!");
 
-document.addEventListener("DOMContentLoaded", function () {
-  const body = document.querySelector("body");
-  console.log(body);
+//gets anything with matching the string "body"
+const body = document.querySelector("body");
+console.log(body);
 
-  // const h1 = document.querySelector("h1");
-  // console.log(h1);
+//gets anything matching the string "h1"
+const h1 = document.querySelector("h1");
+console.log(h1);
 
-  // const h2 = document.querySelector("h2");
-  // h2.textContent = "I changed this using JS";
-});
+const h2 = document.querySelector("h2");
+console.log(h2);
 
-// const body = document.querySelector("body");
-
-// console.log(body);
+//changes the h2 textContent
+const h2Text = document.querySelector("h2");
+h2Text.textContent = "I changed this using JS";
