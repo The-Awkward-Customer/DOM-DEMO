01d9881074e7b408a9603dd812a2c3071518bd49
PETER ABBOTT <abbotsayshi@gmail.com>
2023-12-01 09:17:16 +0000

    init bruv

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("hello there!");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const body = document.querySelector("body");
+  console.log(body);
+
+  const h1 = document.querySelector("h1");
+  console.log(h1);
+
+  const h2 = document.querySelector("h2");
+  h2.textContent = "I changed this using JS";
+});
+
+// const body = document.querySelector("body");
+
+// console.log(body);
